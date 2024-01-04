e6f33e3ebc0cd58b6c5c5a174d5e87f6d94b8031
SandraCiudad <145652781+SandraCiudad@users.noreply.github.com>
2024-01-04 09:56:47 +0100

    Add files via upload

--- sqrt.hpp
+++ sqrt.hpp
@@ -1,6 +1,14 @@
 #ifndef SQUARE_ROOT_HPP__
 #define SQUARE_ROOT_HPP__
 
+/**
+ * @brief calculates the square root of a number
+ * 
+ * @param a is the number whose square root is going to be calculated
+ * 
+ * @return the square root of a
+ *  
+ */
 class sqrt {
 
     public:
